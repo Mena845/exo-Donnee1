0d46c3434d6afd7fd942302d719796c2308dcb4d
mena <tsikiniainatsilavina@gmail.com>
2025-11-13 11:39:44 +0300

    added the seconde answer

--- script.sql
+++ script.sql
@@ -1,4 +1,8 @@
--- 1
+-- 1: l’id, first_name, last_name des employés qui n’ont pas d’équipe. 
 SELECT id,first_name,last_name FROM Employee LEFT JOIN Team t ON Employee.team_id = t.id WHERE t.id IS NULL;
+
+-- 2: l’id, first_name, last_name des employés qui n’ont jamais pris de congé 
+SELECT id, first_name,last_name FROM Employee  LEFT JOIN Leave  ON Employee.id = Leave.employee_id WHERE Leave.id IS NULL;
+
 
 

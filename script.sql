e81db70be41fa22d6f51b754b8a9ee811d82f54b
mena <tsikiniainatsilavina@gmail.com>
2025-11-14 11:49:16 +0300

    added the next answer

--- script.sql
+++ script.sql
@@ -1,8 +1,8 @@
 -- 1: l’id, first_name, last_name des employés qui n’ont pas d’équipe. 
-SELECT id,first_name,last_name FROM Employee LEFT JOIN Team t ON Employee.team_id = t.id WHERE t.id IS NULL;
+ select id , first_name , last_name from Employee where id_Team is null;
 
 -- 2: l’id, first_name, last_name des employés qui n’ont jamais pris de congé 
-SELECT id, first_name,last_name FROM Employee  LEFT JOIN Leave  ON Employee.id = Leave.employee_id WHERE Leave.id IS NULL;
+SELECT id, first_name,last_name FROM Employee where id_Leave is null;
 
-
-
+-- 3: l’id du congé, le début du congé, la fin du congé, le nom & prénom de l’employé qui prend congé et le nom de son équipe.
+select l.id_Leave , l.start_date , l.end_date , e.first_name , e.last_name , t.name_Team from Employee e inner join Leave l on e.id_Leave = l.id_Leave inner join Team t on e.id_Team = t.id_Team;

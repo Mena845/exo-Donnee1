ca86f9bc7d43909e53b2490ca05a9d1606cc7e3f
mena <tsikiniainatsilavina@gmail.com>
2025-11-14 11:54:51 +0300

    added the fourth answer

--- script.sql
+++ script.sql
@@ -6,3 +6,6 @@ SELECT id, first_name,last_name FROM Employee where id_Leave is null;
 
 -- 3: l’id du congé, le début du congé, la fin du congé, le nom & prénom de l’employé qui prend congé et le nom de son équipe.
 select l.id_Leave , l.start_date , l.end_date , e.first_name , e.last_name , t.name_Team from Employee e inner join Leave l on e.id_Leave = l.id_Leave inner join Team t on e.id_Team = t.id_Team;
+
+-- 4: nombre d’employés par contract_type
+select contract_type , count(*) as number_Employee from Employee  group by contract_type;
